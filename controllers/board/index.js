f3cc84c0317a4e19c6854a547f166ddf3d039be2
whdudtod1273 <whdudtod1273@naver.com>
2022-03-06 20:48:16 +0900

    getBoard 수정, tag검색 추가

--- controllers/board/index.js
+++ controllers/board/index.js
@@ -1,24 +1,24 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const ctrl = require('./board.ctrl');
+const ctrl = require("./board.ctrl");
 
-router.get('/', ctrl.get_boards);
+router.get("/", ctrl.get_boards);
 
 // 게시글 CRUD
-router.get('/write/:id', ctrl.get_board);
-router.post('/write', ctrl.post_board);
-router.put('/write/:id', ctrl.put_board);
-router.delete('/delete/:id', ctrl.del_board);
+router.get("/write/:id", ctrl.get_board);
+router.post("/write", ctrl.post_board);
+router.put("/write/:id", ctrl.put_board);
+router.delete("/delete/:id", ctrl.del_board);
 
 // 좋아요
-router.post('/like', ctrl.post_like);
-router.delete('/like', ctrl.delete_like);
+router.post("/like", ctrl.post_like);
+router.delete("/like", ctrl.delete_like);
 
 // 댓글
-router.get('/:id/comment', ctrl.get_comment);
-router.post('/:id/comment', ctrl.post_comment);
+router.get("/:id/comment", ctrl.get_comment);
+router.post("/:id/comment", ctrl.post_comment);
 
-router.get('/explore',ctrl.get_search)
+router.get("/explore", ctrl.get_search);
+router.get("/explore/tag", ctrl.get_searchTag);
 
 module.exports = router;
-
