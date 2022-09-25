1e23c920427eedfcdfcf56871e2ba0f1c4e67123
Young1273 <young@freshcode.me>
2022-09-25 16:15:48 +0900

    add : 로그인, 로그인 조회 스웨거 작성

--- controllers/account/index.js
+++ controllers/account/index.js
@@ -4,8 +4,6 @@ const ctrl = require("./account.ctrl");
 const passport = require("../../middleware/passport-local");
 const upload = require("../../middleware/multer");
 
-router.get("/", ctrl.get_is_login);
-
 /**
  * @swagger
  * components:
@@ -26,7 +24,48 @@ router.get("/", ctrl.get_is_login);
  *        username:
  *          type: string
  *          description: 이름
+ *        phone:
+ *          type: string
+ *          description: 휴대폰 번호
+ *        intro:
+ *          type: string
+ *          description: 인트로
  */
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Login:
+ *      type: object
+ *      required:
+ *        - email
+ *        - password
+ *      properties:
+ *        email:
+ *          type: string
+ *          description: 이메일
+ *        password:
+ *          type: string
+ *          description: 비밀번호
+ */
+
+/**
+ * @swagger
+ *  /api/account:
+ *    get:
+ *      summary : 로그인 조회 api
+ *      tags: [ 로그인 ]
+ *      responses:
+ *        200:
+ *          description: 회원가입 성공
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Login'
+ */
+router.get("/", ctrl.get_is_login);
+
 /**
  * @swagger
  *  /api/account/join:
@@ -48,6 +87,27 @@ router.get("/", ctrl.get_is_login);
  *                $ref: '#/components/schemas/Join'
  */
 router.post("/join", ctrl.post_join);
+
+/**
+ * @swagger
+ *  /api/account/login:
+ *    post:
+ *      summary : 로그인 api
+ *      tags: [ 로그인 ]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Login'
+ *      responses:
+ *        200:
+ *          description: 로그인 성공
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Login'
+ */
 router.post("/login", passport.authenticate("local"), ctrl.post_login);
 router.post("/logout", ctrl.post_logout);
 
