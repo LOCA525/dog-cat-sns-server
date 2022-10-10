6dc2a1e333f982faa6c1d96db4812d2b33ced745
trevor1107 <qnrudwns123@naver.com>
2022-10-10 21:17:54 +0900

    feat: DB 모델 동기화 기능 추가(테이블 없으면, 테이블 생성)

--- models/index.js
+++ models/index.js
@@ -5,39 +5,45 @@ var dotenv = require('dotenv');
 
 dotenv.config(); //LOAD CONFIG
 
-const sequelize = new Sequelize(process.env.DATABASE,
-    process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     timezone: '+09:00', //한국 시간 셋팅
     operatorsAliases: Sequelize.Op,
     pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+  },
+);
 
 let db = [];
 
 fs.readdirSync(__dirname)
-    .filter(file => {
-        // index.js를 제외하고 돌면서 모델안 모델들 생성
-        return file.indexOf('.js') && file !== 'index.js'
-    })
-    .forEach(file => {
-        var model = sequelize.import(path.join(__dirname,
-            file));
-        db[model.name] = model;
-    });
+  .filter(file => {
+    // index.js를 제외하고 돌면서 모델안 모델들 생성
+    return file.indexOf('.js') && file !== 'index.js';
+  })
+  .forEach(file => {
+    var model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach(modelName => {
-    if ("associate" in db[modelName]) {
-        db[modelName].associate(db);
-    }
+  if ('associate' in db[modelName]) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+// db 모델 데이터베이스 동기화, 테이블이 없으면 테이블 생성
+db.sequelize.sync();
 
 module.exports = db;
