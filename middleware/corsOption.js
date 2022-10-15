7688b7602f8b40414db95e613a3112d146516a82
trevor1107 <qnrudwns123@naver.com>
2022-10-15 16:16:58 +0900

    feat: cors white list 3030포트 추가

--- middleware/corsOption.js
+++ middleware/corsOption.js
@@ -1,4 +1,4 @@
-const whitelist = ['http://localhost:3000'];
+const whitelist = ['http://localhost:3000', 'http://localhost:3030'];
 const corsOption = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
